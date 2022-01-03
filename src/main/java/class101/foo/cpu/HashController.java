babcf8a6f42052882014c5123582d1e05d419f73
osandra <okhw97@gmail.com>
2022-01-03 17:37:17 +0900

    change deploy message

--- src/main/java/class101/foo/cpu/HashController.java
+++ src/main/java/class101/foo/cpu/HashController.java
@@ -21,7 +21,7 @@ public class HashController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "hello";
+        return "deploy test ";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
